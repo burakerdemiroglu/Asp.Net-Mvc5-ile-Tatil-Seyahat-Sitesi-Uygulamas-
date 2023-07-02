7e491b6e44dc0d56675fdbbc46e8459d7eb6169a
Burak ERDEMİROĞLU <35409987+burakerdemiroglu@users.noreply.github.com>
2023-07-02 23:17:26 +0300

    Asp.Net.Mvc5-Tatil-Sitesi

---
+++ TravelTripProje/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class ContactController : Controller
+    {
+        Context c = new Context();
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public PartialViewResult MailAl()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult MailAl(iletisim i)
+        { 
+            if (i.AdSoyad != null || i.Mail != null || i.Konu != null || i.Mesaj != null)
+            {
+                TempData["Success"] = "Mailiniz başarılı bir şekilde gönderildi.";
+                c.iletisims.Add(i);
+                c.SaveChanges();
+            }
+            else
+            {
+                ViewData["Error"] = "Mailiniz gönderilemedi. Lütfen alanları doldurunuz."; 
+            }
+            return PartialView();
+        }
+    }
+}
---
+++ TravelTripProje/Migrations/202306301516062_InitialMigration.cs
@@ -0,0 +1,18 @@
+﻿namespace TravelTripProje.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Hakkımızda", "Aciklama", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Hakkımızda", "Aciklama", c => c.Int(nullable: false));
+        }
+    }
+}
